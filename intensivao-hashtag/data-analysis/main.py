2f21f9a327b7846c97b14c87baa5d6d24195761c
Propato <david123propato@gmail.com>
2023-03-17 03:04:34 -0300

    aula 2

--- intensivao-hashtag/data-analysis/main.py
+++ intensivao-hashtag/data-analysis/main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import plotly.express as px
 # Open data base
-table = pd.read_csv('clientes.csv', sep=';', encoding='latin')
+table = pd.read_csv("clientes.csv", sep=';', encoding='latin')
 
 # Delete null column
 table.drop("Unnamed: 8", axis=1, inplace=True)
@@ -16,9 +16,9 @@ table.dropna(inplace=True)
 
 # Create and show the graph for all column
 for column in table.columns:
-    graph = px.histogram(table, x=column, y="Nota (1-100)", histfunc="avg", text_auto=True)
+    graph = px.histogram(table, x=column, y="Nota (1-100)", histfunc="avg", text_auto=True, nbins=8)
     graph.show()
 
-# print(table)
-# print(table.info())
-# print(table.describe())
+print(table)
+print(table.info())
+print(table.describe())
